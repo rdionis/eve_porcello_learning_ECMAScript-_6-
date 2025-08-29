b0c2c7cd6d18a2ee85485fcb2f19bb1e3c68922e
rdionis <dionisio.raquel@gmail.com>
2025-08-29 14:03:59 +0200

    Promises with fetch()

--- Ch06/06_03/start/script.js
+++ Ch06/06_03/start/script.js
@@ -1,23 +1,14 @@
-const spacePeople = () => {
-  return new Promise((resolves, rejects) => {
-    const api =
-      "http://api.open-notify.org/astros.json";
-    const request = new XMLHttpRequest();
-    request.open("GET", api);
-    request.onload = () => {
-      if (request.status === 200) {
-        resolves(JSON.parse(request.response));
-      } else {
-        rejects(Error(request.statusText));
-      }
-    };
-    request.onerror = (err) => rejects(err);
-    request.send();
-  });
-};
+let getSpacePeople = () =>
+  // fetch is a function that works natively in the browser / is built in to every single browser
+  fetch("http://api.open-notify.org/astros.json").then(res => res.json());
 
-spacePeople().then(
-  (spaceData) => console.log(spaceData),
-  (err) =>
-    console.error(new Error("Can't load people"))
-);
+// getSpacePeople().then(data => console.log(data)); //this can be refactored into the code in the line below:
+// getSpacePeople().then(console.log);
+
+let spaceNames = () =>
+  getSpacePeople()
+    .then(json => json.people)
+    .then(people => people.map(p => p.name))
+    .then(names => names.join(", "));
+
+spaceNames().then(console.log);
